e184e10a412dbce128494ec68e29a5de398dcca3
Abdoul <abdoulramanediallo44@gmail.com>
2023-10-26 12:31:03 +0000

    fixing issue

--- spec/models/car_spec.rb
+++ spec/models/car_spec.rb
@@ -1,37 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Car, type: :model do
-  it "is valid with all required attributes" do
-    user = User.create(username: "testuser")
+  it 'is valid with all required attributes' do
+    user = User.create(username: 'testuser')
     car = Car.new(
-      name: "Car 1",
-      image: "car_image.jpg",
-      description: "A nice car",
+      name: 'Car 1',
+      image: 'car_image.jpg',
+      description: 'A nice car',
       deposit: 1000.0,
       finance_fee: 500.0,
       option_to_purchase_fee: 200.0,
-      total_amount_payable: 15000.0,
+      total_amount_payable: 15_000.0,
       duration: 36,
       user_id: user.id
     )
     expect(car).to be_valid
   end
 
-  it "is invalid without a name" do
-    user = User.create(username: "testuser")
+  it 'is invalid without a name' do
+    user = User.create(username: 'testuser')
     car = user.cars.new(name: nil, deposit: 1000.0, finance_fee: 500.0)
     expect(car).not_to be_valid
   end
 
-  it "is invalid without a deposit" do
-    user = User.create(username: "testuser")
-    car = user.cars.new(name: "Car 1", deposit: nil, finance_fee: 500.0)
+  it 'is invalid without a deposit' do
+    user = User.create(username: 'testuser')
+    car = user.cars.new(name: 'Car 1', deposit: nil, finance_fee: 500.0)
     expect(car).not_to be_valid
   end
 
-  it "is invalid without a finance_fee" do
-    user = User.create(username: "testuser")
-    car = user.cars.new(name: "Car 1", deposit: 1000.0, finance_fee: nil)
+  it 'is invalid without a finance_fee' do
+    user = User.create(username: 'testuser')
+    car = user.cars.new(name: 'Car 1', deposit: 1000.0, finance_fee: nil)
     expect(car).not_to be_valid
   end
 end
