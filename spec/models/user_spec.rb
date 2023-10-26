f8d21db4c90031891a0f7005d07d77fbfd115f60
Abdoul <abdoulramanediallo44@gmail.com>
2023-10-26 11:59:45 +0000

    adding model user test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with valid attributes' do
+    user = User.new(username: 'John')
+    expect(user).to be_valid
+  end
+  it 'is not valid with empty string' do
+    user = User.new(username: '')
+    expect(user).not_to be_valid
+  end
+  it 'is not valid with a nil name' do
+    user = User.new(username: nil)
+    expect(user).not_to be_valid
+  end
 end
