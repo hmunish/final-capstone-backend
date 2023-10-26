c38225a069ce44916da40e2424aad17174cbfb50
Abdoul <abdoulramanediallo44@gmail.com>
2023-10-26 12:35:22 +0000

    adding test cases fixed validations

--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Reservation, type: :model do
-  it "is valid with a location and date" do
-    user = User.create(username: "testuser")
+  it 'is valid with a location and date' do
+    user = User.create(username: 'testuser')
     car = Car.new(
       name: 'Car 1',
       image: 'car_image.jpg',
@@ -14,21 +14,21 @@ RSpec.describe Reservation, type: :model do
       duration: 36,
       user_id: user.id
     )
-    reservation = car.reservations.new(location: "Location 1", date: Date.today, car_id: car.id, user_id: user.id)
+    reservation = car.reservations.new(location: 'Location 1', date: Date.today, car_id: car.id, user_id: user.id)
     expect(reservation).to be_valid
   end
 
-  it "is invalid without a location" do
-    user = User.create(username: "testuser")
-    car = user.cars.create(name: "Car 1", deposit: 1000.0, finance_fee: 500.0)
+  it 'is invalid without a location' do
+    user = User.create(username: 'testuser')
+    car = user.cars.create(name: 'Car 1', deposit: 1000.0, finance_fee: 500.0)
     reservation = car.reservations.new(location: nil, date: Date.today)
     expect(reservation).not_to be_valid
   end
 
-  it "is invalid without a date" do
-    user = User.create(username: "testuser")
-    car = user.cars.create(name: "Car 1", deposit: 1000.0, finance_fee: 500.0)
-    reservation = car.reservations.new(location: "Location 1", date: nil)
+  it 'is invalid without a date' do
+    user = User.create(username: 'testuser')
+    car = user.cars.create(name: 'Car 1', deposit: 1000.0, finance_fee: 500.0)
+    reservation = car.reservations.new(location: 'Location 1', date: nil)
     expect(reservation).not_to be_valid
   end
 end
