d987d41f12e30af85e555430c28417ee56fc41d9
Sunga Thawethe <sungabt@gmail.com>
2023-10-26 13:31:29 +0200

    Add validations

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,10 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :car
+
+  # Validations
+
+  validates :location, presence: :true
+  validates :date, presence: :true
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
   has_many :cars
   has_many :reservations
+
+
+  # Validations 
+
+  validates :username, presence: :true
 end
