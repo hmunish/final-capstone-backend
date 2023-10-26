cfa425464bbd6cfa842578eeb81d58a3491fcab1
Munish <hmunish5@gmail.com>
2023-10-26 16:23:49 +0200

    Fix rubocop errors

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,24 +1,23 @@
 class Api::V1::UsersController < ApplicationController
-    def index
-        @user = User.find_by(username: 'testUser2')
-        # @user = User.find_by(name: user_params[:name])
-    
-        if @user
-          render json: {
-            status: { code: 200, message: 'signed in successfuly', data: @user }
-          }, status: :ok
-        else
-          render json: { error: 'User not found' }, status: :not_found
-        end
-      end
-    
-      private
-    
-      def user_params
-        params.require(:user).permit(:name)
-      end
-end
+  def index
+    @user = User.find_by(username: 'testUser2')
+    # @user = User.find_by(name: user_params[:name])
 
+    if @user
+      render json: {
+        status: { code: 200, message: 'signed in successfuly', data: @user }
+      }, status: :ok
+    else
+      render json: { error: 'User not found' }, status: :not_found
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name)
+  end
+end
 
 # Code to be used in future
 
