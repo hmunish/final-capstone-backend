49f01ed9eb62b61fef89a557824c33fc700d2023
Abdoul <abdoulramanediallo44@gmail.com>
2023-10-27 10:05:53 +0000

    adding cars controllers api

---
+++ app/controllers/api/v1/cars_controller.rb
@@ -0,0 +1,32 @@
+class Api::V1::CarsController < ApplicationController
+  before_action :set_user, only: %i[show]
+
+  def index
+    @cars = Car.all
+    render json: @cars
+  end
+
+  def show
+    render json: @car
+  end
+
+  def create
+    @car = current_user.cars.new(car_params)
+    if @car.save
+        render json: @car, status: :created
+    else
+        render json: @car.errors, status: :unprocessable_entity
+    end
+  end
+
+
+  private
+
+  def set_user
+    @car = Car.find(params[:id])
+  end
+
+  def car_params
+    params.require(:car).permit(:name, :image, :description, :deposit, :finace_fee, :option_to_purchase_fee, :total_amount_payable, :duration)
+  end
+end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,83 +1,59 @@
 class Api::V1::UsersController < ApplicationController
-  before_action :set_user, only: [:show, :update, :destroy]
+  before_action :set_user, only: %i[show update destroy]
 
-    def index
-      @users = User.all
+  def index
+    @users = User.all
 
-      if @users
-        render json: { status: { code: 200, message: 'signed in successfuly', data: @user }}, status: :ok
-      else
-              render json: { error: 'Users not found' }, status: :not_found
-      end
+    if @users
+      render json: { status: { code: 200, message: 'signed in successfuly', data: @user } }, status: :ok
+    else
+      render json: { error: 'Users not found' }, status: :not_found
     end
+  end
 
-    def show
+  def show
+    render json: @user
+  end
+
+  def create
+    @user = User.new(username: params[:username])
+
+    if @user.save
+      render json: @user, status: :created
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @user.update(user_params)
       render json: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
     end
+  end
 
-    def create
-      @user = User.new(username: params[:username])
+  def destroy
+    @user.destroy
+    head :no_content
+  end
 
-      if @user.save
-        render json: @user, status: :created
-      else
-        render json: @user.errors, status: :unprocessable_entity
-      end
-    end
+  # def authenticate
+  #   @user = User.find_by(username: params[:username])
+  #   if @user
+  #     render json: { message: "Authentication successful"}, status: :ok
+  #   else
+  #     render json: { message: "Invalid Username"}, status: :unauthorized
+  #   end
+  # end
 
-    def update
-      if @user.update(user_params)
-        render json: @user
-      else
-        render json: @user.errors, status: :unprocessable_entity
-      end
-    end
+  private
 
-    def destroy
-      @user.destroy
-      head :no_content
-    end
+  def set_user
+    @user = User.find(params[:id])
+  end
 
-    # def authenticate
-    #   @user = User.find_by(username: params[:username])
-    #   if @user
-    #     render json: { message: "Authentication successful"}, status: :ok 
-    #   else
-    #     render json: { message: "Invalid Username"}, status: :unauthorized
-    #   end
-    # end
-
-    private
-
-    def set_user
-      @user = User.find(params[:id])
-    end
-
-    def user_params
-      params.require(:user).permit(:username)
-    end
-
+  def user_params
+    params.require(:user).permit(:username)
+  end
 end
-
-# def index
-#   @user = User.find_by(username: 'testUser2')
-#   # @user = User.find_by(name: user_params[:name])
-
-#   if @user
-#     render json: {
-#       status: { code: 200, message: 'signed in successfuly', data: @user }
-#     }, status: :ok
-#   else
-#     render json: { error: 'User not found' }, status: :not_found
-#   end
-# end
-
-# private
-
-# def user_params
-#   params.require(:user).permit(:name)
-# end
-
-# # Code to be used in future
-
-
