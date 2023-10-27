39bfc6669a634f373cd5a12ad630f58634ceb368
Abdoul <abdoulramanediallo44@gmail.com>
2023-10-27 12:51:12 +0000

    config routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
     namespace :api do
         namespace :v1 do
-            resources :users
-            resources :cars
+            resources :users do
+                resources :cars
+            end
+            
             # POST '/authenticate', to: 'authenticate'
         end
     end
