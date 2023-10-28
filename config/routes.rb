cf8e2037f27371da7c6258a9c698617a1bea8cb6
Abdoul <abdoulramanediallo44@gmail.com>
2023-10-28 19:51:59 +0000

    update routes to match the changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
     namespace :api do
         namespace :v1 do
             resources :users, only: [:create, :show] do
-                resources :cars
+                resources :cars, only: [:create, :show, :destroy]
             end
             post 'login', to: 'authentication#login'         
         end
