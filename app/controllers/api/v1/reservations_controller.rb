938714fe96319c50553e74623d4c296cdd11ea8f
Faranosh Amini <faranosh.amini4@gmail.com>
2023-10-30 21:25:34 +0430

    Add index method in reservations_controller

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,4 +1,12 @@
 class Api::V1::ReservationsController < ApplicationController
   skip_before_action :authenticate, only: %i[index create show update destroy]
   before_action :set_reservation, only: %i[show update destroy]
+  def index
+    @reservations = Reservation.order(created_at: :desc).all
+    if @reservations
+      render json: { status: { code: 200, message: 'Reservations retrieved successfully.', data: @reservations } }, status: :ok
+    else
+      render json: { error: 'Reservations not found.' }, status: :not_found
+    end
+  end
 end
