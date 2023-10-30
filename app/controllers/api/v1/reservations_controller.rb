7293a0615bd4c9a8e5c825d0c321c4f228f2bb60
Abdoul <abdoulramanediallo44@gmail.com>
2023-10-30 08:46:43 +0000

    genrate reservations controller

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,2 +1,4 @@
 class Api::V1::ReservationsController < ApplicationController
+  skip_before_action :authenticate, only: %i[index create show update destroy]
+  before_action :set_reservation, only: %i[show update destroy]
 end
