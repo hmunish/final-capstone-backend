f36ed41f0dbdaa6306d9bbb2fc12816aa33a63ce
Faranosh Amini <faranosh.amini4@gmail.com>
2023-10-30 22:06:58 +0430

    Update reservations_controller.rb

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::ReservationsController < ApplicationController
-  skip_before_action :authenticate, only: %i[index create show update destroy]
+  before_action :authenticate, only: %i[index create show update destroy]
   before_action :set_reservation, only: %i[show update destroy]
   def index
     @reservations = Reservation.order(created_at: :desc).all
