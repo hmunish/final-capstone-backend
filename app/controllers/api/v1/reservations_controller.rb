b3c04a495b2c42fed737646f27a2d0a66cce269e
Abdoul <abdoulramanediallo44@gmail.com>
2023-10-31 06:29:01 +0000

    making changes required by the reviewer

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,8 +1,10 @@
 class Api::V1::ReservationsController < ApplicationController
   before_action :authenticate, only: %i[index create show update destroy]
   before_action :set_reservation, only: %i[show update destroy]
+
   def index
-    @reservations = Reservation.order(created_at: :desc).all
+    current_user = User.find(params[:user_id])
+    @reservations = current_user.reservations.order(created_at: :desc).all
     if @reservations
       render json: { status: { code: 200, message: 'Reservations retrieved successfully.', data: @reservations } }, status: :ok
     else
